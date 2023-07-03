56d7ced82dd61fc47744381bc4ece3f4640ba17c
artemkacevic <akatsevich@live.ru>
2023-07-03 22:38:29 +0800

    New bean added

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class Dog extends Animal{
+    public String toString() {
+        return "Im a Dog";
+    }
+
+
+}
